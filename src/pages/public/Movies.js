15b6e491c1a559a8b3482496b777550af72d1602
RimgaudasGreenhat <rimgaudasAER@gmail.com>
2025-07-09 20:43:47 +0300

    up

--- src/pages/public/Movies.js
+++ src/pages/public/Movies.js
@@ -1,7 +1,7 @@
 import { moviesData } from "../../data/moviesData.js";
 import { PageTemplate } from "../../templates/PageTemplate.js";
 import { moviesFilterForm } from "../../ui/forms/moviesFilterForm.js";
-import { moviesListSection } from "../../ui/moviesList.js";
+import { moviesListSection } from "../../../src/ui/movieList.js";
 import { pageTitle } from "../../ui/pageTitle.js";
 
 export class PageMovies extends PageTemplate {
